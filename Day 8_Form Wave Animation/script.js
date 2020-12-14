57cad0b26d7d8c3488ffd91c5b8fcaf82661aadf
Ylva Turner <ylva.e.thorsen@gmail.com>
2020-12-14 22:50:56 +0000

    add wave effect

--- Day 8_Form Wave Animation/script.js
+++ Day 8_Form Wave Animation/script.js
@@ -1,1 +1,9 @@
-// code goes here
+const labels = document.querySelectorAll('.form-control label')
+
+
+labels.forEach(label => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 60}ms">${letter}</span>`)
+    .join('')
+})
