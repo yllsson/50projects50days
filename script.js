5db2d02779b0378c81f724130639a0c1aef58df2
Ylva Turner <ylva.e.thorsen@gmail.com>
2020-12-18 22:51:28 +0000

    Add entry generator function
    Created a function that generates new entries on my page, based on an array of the projects. So now, instead of copy-pasting and changing the code on the index.html page, I can simply add the name of the project in the array and the entry will show on my progress page!

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// this bit of code allows me to add the name of the project into the array and stop copy-pasting the same code over and over on my HTML page
+
+const projects = [
+  'Expanding Cards', 
+  'Progress Steps', 
+  'Rotating Navigation', 
+  'Hidden Search Widget', 
+  'Blurry Loading', 
+  'Scroll Animation', 
+  'Split Landing Page',
+  'Form Wave Animation',
+  'Sound Board'
+]
+const container = document.querySelector('.container');
+
+projects.forEach(project => {
+  const entry = document.createElement('div');
+  const link = document.createElement('a');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+
+  entry.classList.add('entry');
+
+  link.setAttribute('href', `./Day ${projects.indexOf(project)+1}_${project}/index.html`)
+
+  h3.innerText = `Day ${projects.indexOf(project)+1}: ${project}`
+  p.innerText = `Click to see preview`
+
+  
+  container.appendChild(entry);
+  entry.appendChild(link);
+  link.appendChild(h3);
+  entry.appendChild(p)
+})
+
+
+// this part adds a button-expanding animation to my entry list
+
 const entries = document.querySelectorAll('.entry');
 
 entries.forEach(entry => {
@@ -13,3 +51,4 @@ entries.forEach(entry => {
 function removeActiveClass() {
   entries.forEach(entry => entry.classList.remove('active'));
 }
+
