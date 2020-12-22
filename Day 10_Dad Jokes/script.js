e73dda38394b30bfbf9116aab66eef4f9ca8202c
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2020-12-22 21:44:01 +0000

    script/api request added

--- Day 10_Dad Jokes/script.js
+++ Day 10_Dad Jokes/script.js
@@ -1,1 +1,38 @@
-// code goes here
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// async/await option below //
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const response = await fetch('https://icanhazdadjoke.com/', config);
+
+  const data = await response.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+// .then() option below //
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
