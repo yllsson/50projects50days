23de4276ab939d6c3d1bcd713c316cf543719da7
Ylva Turner <ylva.e.thorsen@gmail.com>
2020-12-28 16:17:24 +0000

    add script

--- Day 11_Event KeyCodes/script.js
+++ Day 11_Event KeyCodes/script.js
@@ -1,1 +1,20 @@
-// code goes here
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (event) => {
+  insert.innerHTML = `
+  <div class="key">
+      ${event.key === ' ' ? 'Space' : event.key}
+      <small>event.key</small>
+    </div>
+
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
+
+    <div class="key">
+      ${event.code}
+      <small>event.code</small>
+    </div>
+  `;
+});
