7ac84616a497d581adea5bc4e1faa4fa85b68d99
Ylva Turner <ylva.e.thorsen@gmail.com>
2020-12-28 17:23:43 +0000

    reamending button flex

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ to generate a new entry on the homepage,
 and stop copy-pasting the same code over and over in index.html */
 
 const projects = [
-  'Expanding Cards', 
-  'Progress Steps', 
-  'Rotating Navigation', 
-  'Hidden Search Widget', 
-  'Blurry Loading', 
-  'Scroll Animation', 
+  'Expanding Cards',
+  'Progress Steps',
+  'Rotating Navigation',
+  'Hidden Search Widget',
+  'Blurry Loading',
+  'Scroll Animation',
   'Split Landing Page',
   'Form Wave Animation',
   'Sound Board',
   'Dad Jokes',
-  'Event KeyCodes'
-]
+  'Event KeyCodes',
+];
 const container = document.querySelector('.container');
 
-projects.forEach(project => {
+projects.forEach((project) => {
   const entry = document.createElement('div');
   const link = document.createElement('a');
   const h3 = document.createElement('h3');
@@ -25,33 +25,34 @@ projects.forEach(project => {
 
   entry.classList.add('entry');
 
-  link.setAttribute('href', `./Day ${projects.indexOf(project)+1}_${project}/index.html`)
+  link.setAttribute(
+    'href',
+    `./Day ${projects.indexOf(project) + 1}_${project}/index.html`
+  );
 
-  h3.innerText = `Day ${projects.indexOf(project)+1}: ${project}`
-  p.innerText = `Click to see preview`
-  
+  h3.innerText = `Day ${projects.indexOf(project) + 1}: ${project}`;
+  p.innerText = `Click to see preview`;
+
   container.appendChild(entry);
   entry.appendChild(link);
   link.appendChild(h3);
-  entry.appendChild(p)
-})
-
+  entry.appendChild(p);
+});
 
 // this part adds a button-expanding animation to my entry list
 
 const entries = document.querySelectorAll('.entry');
 
-entries.forEach(entry => {
+entries.forEach((entry) => {
   entry.addEventListener('mouseover', () => {
     entry.classList.add('active');
-  })
+  });
 
   entry.addEventListener('mouseleave', () => {
     removeActiveClass();
-  })
-})
+  });
+});
 
 function removeActiveClass() {
-  entries.forEach(entry => entry.classList.remove('active'));
+  entries.forEach((entry) => entry.classList.remove('active'));
 }
-
