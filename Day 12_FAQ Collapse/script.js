3131b3c0572614aa3df0ee2f17b1f98d1c84c712
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-05 20:43:56 +0000

    add toggle script

--- Day 12_FAQ Collapse/script.js
+++ Day 12_FAQ Collapse/script.js
@@ -1,1 +1,22 @@
-// code goes here
+// my solution
+
+const buttons = document.querySelectorAll('.faq-toggle');
+const faqs = document.querySelectorAll('.faq');
+
+buttons.forEach((button, idx) => {
+  button.addEventListener('click', () => {
+    faqs[idx].classList.toggle('active');
+  });
+});
+
+// course solution
+
+/*
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active');
+  });
+});
+*/
