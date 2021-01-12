53a3afd2f3d14140a17b84358a09f1d8f045d18e
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-12 21:22:06 +0000

    add random choice and highlighting scripts

--- Day 13_Random Choice Picker/script.js
+++ Day 13_Random Choice Picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
+
+  if (e.key == 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 let createTags = (input) => {
@@ -21,4 +29,40 @@ let createTags = (input) => {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+};
+
+let randomSelect = () => {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+};
+
+let pickRandomTag = () => {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+};
+
+let highlightTag = (tag) => {
+  tag.classList.add('highlight');
+};
+
+let unHighlightTag = (tag) => {
+  tag.classList.remove('highlight');
 };
