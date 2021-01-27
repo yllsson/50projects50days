ee595dd3f3e63028af4090dc98eeae29d003e7f4
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-27 20:37:41 +0000

    add API + API fetching functions

--- Day 17_Movie App/script.js
+++ Day 17_Movie App/script.js
@@ -1,1 +1,32 @@
-// code goes here
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6303983f3d638f003969d9624cd4366c&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=6303983f3d638f003969d9624cd4366c&query="';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
